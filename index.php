3aa78fcc685d29d33abe130774b45aa1a69d6d91
castilloderek445 <102875395+castilloderek445@users.noreply.github.com>
2023-02-23 18:51:57 -0600

    Add files via upload
    I updated navbar, login, signup, getquote (changed html files to php), style.css , some images, getQuote.js
    I added index, style_index.css
    
    getQuote.js is unfinished

---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- Works as guest or logged in as client -->
+<!-- idk what to put in here lol -->
+<?php
+    include_once 'navbar.php';
+?>
+
+<title>Index</title>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>index</title>
+        <link rel="stylesheet" href="style_index.css">
+    </head>
+    <body>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="getQuote.php">Get Quote</a></li>
+                <li><a href="AllQuotes.php">Quote History</a></li>
+                <li><a href="#">Log Out</a></li> 
+                <!-- this will switch from log in/logout depending on the session -->
+            </ul>
+        </nav>
+    </body>
+</html>
