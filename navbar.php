ddacb54e72ab6905e122e8f759ce56dc392dec5f
castilloderek445 <102875395+castilloderek445@users.noreply.github.com>
2023-02-24 17:35:22 -0600

    a ref= getQuote.php ----> getQuotenew.php

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
                 <li class="first"><a>COSC 4353 Team 1 Project</a></li>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="#">Profile</a></li>
-                <li><a href="getQuote.php">Get Quote</a></li>
+                <li><a href="getQuotenew.php">Get Quote</a></li>
                 <li><a href="AllQuotes.php">Quote History</a></li>
                 <li class="last"><a href="#">Log Out</a></li> 
                 <!-- this will switch from log in/logout depending on the session -->
