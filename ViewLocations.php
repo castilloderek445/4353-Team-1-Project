4ace5db39c20823a2778157d5a54588568b76464
JDuarte19 <72234094+JDuarte19@users.noreply.github.com>
2023-03-24 16:40:45 -0500

    Add files via upload

--- ViewLocations.php
+++ ViewLocations.php
@@ -36,7 +36,7 @@
                 <input type="text" placeholder="Enter Zipcode" name="zip">
                 <input type="text" placeholder="Enter City" name="city">
                 <br>
-                <input type="range" min="1" max="6" value="3" step=".01" name="price">
+                <input type="range" min="1" max="6" value="0" step=".01" name="price">
                 <br>
                 <select name="state"> State: 
                     <option value="sel_state" selected>Select State</option>
