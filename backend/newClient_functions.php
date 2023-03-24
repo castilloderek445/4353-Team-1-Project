80089b8c122bd1cc8bebd6d87007b6ef82f7ea21
castilloderek445 <102875395+castilloderek445@users.noreply.github.com>
2023-03-24 12:22:15 -0500

    Add files via upload

--- backend/newClient_functions.php
+++ backend/newClient_functions.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App;
+
     // if any of the fields are empty, throw error
     function emptySignupInput($email, $name, $state, $zip, $pwd, $pwdRepeat){
         $flag; //idk why unset variables shows red squiggly line but it works
@@ -43,7 +45,7 @@
     // if zip is not proper format, throw error
     function invalidZip($zip){ 
         $flag;
-        if(!preg_match("#[0-9]{5}#", $zip)){
+        if(!preg_match('/^\d{5}$/', $zip)){
             $flag = true;
         }
         else{
@@ -64,10 +66,57 @@
         return $flag;
     }
 
-    //stuff that'll add new client info to database
-    // function createNewClient($conn, $email, $name, $state, $zip, $pwd){
+    function uidExists($con, $name, $email){
+        
+        $sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";
+        
+        //prepared statement
+        $stmt = mysqli_stmt_init($con);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("location: ../NewClient.php?error=stmtfailed");
+            exit();
+        }
 
-    //     $sql = "INSERT INTO users ($email, $name, $state, $zip, $pwd) VALUES (?,?,?,?,?)";
-    // }
+        //args: prepared statement, type of data being submitted, data being submitted
+        mysqli_stmt_bind_param($stmt, "ss", $name, $email);
+        mysqli_stmt_execute($stmt);
+
+        $resultData = mysqli_stmt_get_result($stmt);
+
+        if($row = mysqli_fetch_assoc($resultData)){ //if there's data in the db with this username, grab data with username
+            return $row; //returning all data from user if username exists in db
+        }
+        else{
+            $result = false;
+            return $result;
+        }
+
+        //close statement
+        mysqli_stmt_close($stmt);
+
+    }
+
+    //stuff that'll add new client info to database
+    function createNewClient($con, $email, $name, $state, $zip, $pwd){
+
+        $sql = "INSERT INTO users ($email, $name, $state, $zip, $pwd) VALUES (?,?,?,?,?)";
+        $stmt = mysqli_stmt_init($con);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("location: ../NewClient.php?error=stmtfailed");
+            exit();
+        }
+
+        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+        //args: prepared statement, type of data being submitted, data being submitted
+        mysqli_stmt_bind_param($stmt, "sssss", $email, $name, $state, $zip, $hashedPwd);
+        mysqli_stmt_execute($stmt);
+
+        //close statement
+        mysqli_stmt_close($stmt);
+
+        //send user back somewhere
+        header("location: ../NewClient.php?error=none");
+    }
 
 ?>
