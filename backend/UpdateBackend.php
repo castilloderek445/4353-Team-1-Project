1d774023e62114add21a6dd4fae7d1556882df88
JDuarte19 <72234094+JDuarte19@users.noreply.github.com>
2023-04-28 10:49:36 -0500

    Add files via upload

--- backend/UpdateBackend.php
+++ backend/UpdateBackend.php
@@ -17,40 +17,52 @@
         if(empty($street) == false){
             $sqlName = $sql . "Street='" . $street. "'" . " WHERE User_id=" . $_POST['current_id'];
             if (mysqli_query($connection, $sqlName)) {
-                //echo "Record updated successfully";
+                $errorMsg[0] = 'Updated street';
             } 
             else {
                 echo "Error updating record: " . mysqli_error($connection);
             }
-
         }
         if(empty($city) == false){
             $sqlName = $sql . "City='" . $city. "'" . " WHERE User_id=" . $_POST['current_id'];
-            if (mysqli_query($connection, $sqlName)) {
-                //echo "Record updated successfully";
-            } 
-            else {
-                echo "Error updating record: " . mysqli_error($connection);
+            if ((strlen($city) > 40) || preg_match('~[0-9]+~', $city)) {
+                $errorMsg[1] = 'Invalid city';
+            }
+            else{
+                if (mysqli_query($connection, $sqlName)) {
+                    $errorMsg[1] = 'Updated city';
+                } 
+                else {
+                    echo "Error updating record: " . mysqli_error($connection);
+                }
             }
 
         }
         if($state != 'sel_state'){
             $sqlName = $sql . "State='" . $state. "'" . " WHERE User_id=" . $_POST['current_id'];
             if (mysqli_query($connection, $sqlName)) {
-                //echo "Record updated successfully";
+                $errorMsg[2] = 'Updated state';
             } 
             else {
                 echo "Error updating record: " . mysqli_error($connection);
             }
 
         }
+        else{
+            $errorMsg[2] = 'State required';
+        }
         if(empty($zip) == false){
             $sqlName = $sql . "Zip='" . $zip. "'" . " WHERE User_id=" . $_POST['current_id'];
-            if (mysqli_query($connection, $sqlName)) {
-                //echo "Record updated successfully";
-            } 
-            else {
-                echo "Error updating record: " . mysqli_error($connection);
+            if (!preg_match('/^\d{5}$/', $zip)) {
+                $errorMsg[3] = 'Invalid zip code';
+            }
+            else{
+                if (mysqli_query($connection, $sqlName)) {
+                    $errorMsg[3] = 'Updated zip code';
+                } 
+                else {
+                    echo "Error updating record: " . mysqli_error($connection);
+                }
             }
 
         }
